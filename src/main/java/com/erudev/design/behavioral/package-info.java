c8fabc3fe8767196f3c9dbffa3a8a4342c5e8cff
zhaopengfei <1027926875@qq.com>
2020-12-12 10:18:04 +0800

    添加描述信息

--- src/main/java/com/erudev/design/behavioral/package-info.java
+++ src/main/java/com/erudev/design/behavioral/package-info.java
@@ -1,19 +1,10 @@
 /**
- * <p>
  * 行为型模式，包含模式
- * <ul>
- * <li>职责链模式（Chain of Responsibility）重要程度：3（5为满分）</li>
- * <li>命令模式（Command）重要程度：4（5为满分）</li>
- * <li>解释器模式（Interpreter）重要程度：1（5为满分）</li>
- * <li>迭代器模式（Iterator）重要程度：5（5为满分）</li>
- * <li>中介者模式（Mediator）重要程度：2（5为满分）</li>
- * <li>备忘录模式（Memento）重要程度：2（5为满分）</li>
- * <li>观察者模式（Observer）重要程度：5（5为满分）</li>
- * <li>状态模式（State）重要程度：3（5为满分）</li>
- * <li>策略模式（Strategy）重要程度：4（5为满分）</li>
- * <li>模板方法模式（Template Method）重要程度：3（5为满分）</li>
- * <li>访问者模式（Visitor）重要程度：1（5为满分）</li>
- * <li>委派模式（Delegate）重要程度：3（5为满分）</li>
- * </p>
+ *
+ * 职责链模式（Chain of Responsibility）
+ * 迭代器模式（Iterator）
+ * 观察者模式（Observer）
+ * 策略模式（Strategy）
+ * 模板方法模式（Template Method）
  */
 package com.erudev.design.behavioral;
---
+++ src/main/java/com/erudev/design/creational/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 创建型
+ *
+ * 单例模式
+ * 工厂模式（工厂方法和抽象工厂）
+ * 建造者模式
+ */
+package com.erudev.design.creational;
